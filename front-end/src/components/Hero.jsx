c14147acc1e010aaea1c2dd27c2992883a270be8
MNDIOM <mamadou.johm@gmail.com>
2024-08-23 04:43:38 -0400

    added some questionaire, fixed tailwind css added footer added navbar added login form

--- front-end/src/components/Hero.jsx
+++ front-end/src/components/Hero.jsx
@@ -1,5 +1,5 @@
-// Hero.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="container mx-auto text-center">
         <h1 className="text-4xl font-bold mb-4">Welcome to Our Website</h1>
         <p className="text-lg mb-8">We are excited to have you here. Explore our features and get started!</p>
-        <a href="#get-started" className="btn btn-primary">Get Started</a>
+        <Link to="/questionnaire" className="btn btn-primary">Get Started</Link>
       </div>
     </section>
   );
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,13 +1,13 @@
 import daisyui from "daisyui";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/*/.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Corrected this line
   ],
   theme: {
     extend: {},
   },
   plugins: [daisyui],
-}
-
+};
