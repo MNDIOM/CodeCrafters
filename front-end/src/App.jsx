98282987f49b35d925c3e5e9d69b525b9e22ea3f
MNDIOM <mamadou.johm@gmail.com>
2024-08-23 06:24:03 -0400

    hero background image added about us text added( to be improved)

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,21 +4,25 @@ import Hero from './components/Hero';
 import Questionnaire from './components/Questionnaire';
 import Footer from './components/Footer';
 import Login from './components/Login'; // Import the Login component
+import About from './pages/About';
+import SolarInfos from './pages/SolarInfos';
+import Contact from './pages/Contact';
 import './index.css';
 
 function App() {
   return (
     <Router>
-      <Navbar /> {/* Add Navbar component */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Hero />} />
         <Route path="/questionnaire" element={<Questionnaire />} />
-        <Route path="/login" element={<Login />} /> {/* Add route for Login */}
-        {/* Add other routes as needed */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/solarInfos" element={<SolarInfos />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
   );
 }
-
 export default App;
--- front-end/src/components/Hero.jsx
+++ front-end/src/components/Hero.jsx
@@ -1,13 +1,14 @@
+// Hero.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
+import '../index.css'; // Adjust the path as needed
 
 const Hero = () => {
   return (
-    <section className="hero bg-gray-100 p-8">
+    <section className="hero">
       <div className="container mx-auto text-center">
         <h1 className="text-4xl font-bold mb-4">Welcome to Our Website</h1>
         <p className="text-lg mb-8">We are excited to have you here. Explore our features and get started!</p>
-        <Link to="/questionnaire" className="btn btn-primary">Get Started</Link>
+        <a href="/questionnaire" className="btn btn-primary">Get Started</a>
       </div>
     </section>
   );
